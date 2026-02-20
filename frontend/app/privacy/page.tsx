2033fd8a1ff628c5cf97a65f53bdba6be2ff4d7a
ayo-ola0710 <israelolatunle2015@gmail.com>
2026-02-20 17:41:23 +0100

    created the terms and privacy pages placeholder

---
+++ frontend/app/privacy/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>These is the privacy policies page</div>;
+};
+
+export default page;
---
+++ frontend/app/terms/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>These is the terms and condition page</div>;
+};
+
+export default page;
