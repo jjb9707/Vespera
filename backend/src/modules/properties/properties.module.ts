edd0f8e3c2641a42a052efe829f325ac9c21a1e0
Chioma Contributor <contributor@chioma.dev>
2026-03-25 19:50:05 +0100

    Merge branch 'main' of https://github.com/OsagieCynthia/chioma into feature/kyc-encryption-tests

--- backend/src/modules/properties/properties.module.ts
+++ backend/src/modules/properties/properties.module.ts
@@ -1,23 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 import { PropertiesController } from './properties.controller';
 import { PropertiesService } from './properties.service';
+import { PropertyCacheWarmingService } from './property-cache-warming.service';
 import { Property } from './entities/property.entity';
 import { PropertyImage } from './entities/property-image.entity';
 import { PropertyAmenity } from './entities/property-amenity.entity';
 import { RentalUnit } from './entities/rental-unit.entity';
+import { PropertyListingDraft } from './entities/property-listing-draft.entity';
 
 @Module({
   imports: [
+    ScheduleModule,
     TypeOrmModule.forFeature([
       Property,
       PropertyImage,
       PropertyAmenity,
       RentalUnit,
+      PropertyListingDraft,
     ]),
   ],
   controllers: [PropertiesController],
-  providers: [PropertiesService],
+  providers: [PropertiesService, PropertyCacheWarmingService],
   exports: [PropertiesService],
 })
 export class PropertiesModule {}
